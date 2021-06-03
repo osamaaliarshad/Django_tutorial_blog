e1e438ada35223fbd9e4bf1087937efe6ee30147
oali <oaliarshad@gmail.com>
2021-06-02 18:20:28 -0700

    checkpoint to previous version

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -2,25 +2,26 @@ from django.shortcuts import render
 
 posts = [
     {
-        'author': 'Osama',
+        'author': 'CoreyMS',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'May 17, 2021'
-
+        'date_posted': 'August 27, 2018'
     },
     {
-        'author': 'Bobo',
+        'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'May 18, 2021'
-
-    },
+        'date_posted': 'August 28, 2018'
+    }
 ]
+
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': posts
     }
-    return render(request,'blog/home.html', context)
+    return render(request, 'blog/home.html', context)
+
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
