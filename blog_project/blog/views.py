06a6c1fcf0da0786de01b08e3a0a5f6adbdc3009
oali <oaliarshad@gmail.com>
2021-07-01 19:48:58 -0700

    Added details post screen

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 from .models import Post
 
 def home(request):
@@ -7,6 +12,18 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
